cd0f1c683883d917abc1b54dd7ce74b160ed45c1
alisantosun <alisan.tsn@gmail.com>
2024-08-02 23:28:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ _,but,_ = main.columns([1,2,1])
 if but.button("KarbonAyak İzini Hesapla!", type="primary"):
     click_element('tab-1')
 
-tab1, tab2, tab3, tab4, tab5 = comps.tabs(["👴 Personal","🚗 Travel","🗑️ Waste","⚡ Energy","💸 Consumption"])
+tab1, tab2, tab3, tab4, tab5 = comps.tabs(["🧍‍♂️ kişisel","🚎 Seyahat","🚮 Atık","⚡ Enerji","💰 Tüketim"])
 tab_result,_ = result.tabs([" "," "])
 
 def component():
