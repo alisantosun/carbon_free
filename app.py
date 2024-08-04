7fd9558703aef30271135d3993a482d81eba804c
alisantosun <alisan.tsn@gmail.com>
2024-08-04 21:09:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import base64
 from functions import *
 
 st.set_page_config(layout="wide",page_title="Carbon Footprint Calculator", page_icon="./media/favicon.ico")
-    if uploaded_file is not None:
+if uploaded_file is not None:
     # Read the CSV file
     df = pd.read_csv(uploaded_file)
 
